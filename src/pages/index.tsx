082df5a7dce68fb551ab52feebf67deda70a9dd0
SangrishS <97099178+SangrishS@users.noreply.github.com>
2024-08-13 22:47:22 +0530

    api changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     description: '',
 };
 
-const API_ENDPOINT = "http://98.80.18.248:8000";
+const API_ENDPOINT = "http://98.80.18.248:8000/";
 
 const HomePage: NextPage = () => {
     const [filter, setFilter] = useState({
